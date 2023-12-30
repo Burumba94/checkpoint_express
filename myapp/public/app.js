3f13a4f55a28930ee8ad012fcd922a6fe00532d3
Benoît <benoitsagna7@gmail.com>
2023-12-30 23:08:02 +0000

    first commit express

---
+++ myapp/public/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+//Middleware pour checker l'heure de la requête
+const checkWorkingHours = (req, res, next) => {
+  const now = new Date();
+  const dayOfWeek = now.getDate(); 
+  const hourOfWeek = now.getHours();
+
+  if (dayOfWeek >= 1 && dayOfWeek <= 6 && hourOfWeek >= 9 && hourOfWeek < 1){
+    next();
+  } else {
+    res.send('Le site est disponible uniquement pendant les heures et les jours ouvrables !');
+  }
+};
+
+app.use(checkWorkingHours);
+app.use(express.static('public'));
+
+//Template Pug
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+//routes
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Accueil'});
+});
+
+app.get('/services', (req, res) => {
+  res.render('services', {title: 'Nos Services'});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {title: 'Nous Contacter'});
+});
+
+app.listen(port, () => {
+  console.log('Le serveur écoute sur le port ${port}');
+});
